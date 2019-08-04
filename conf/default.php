74a2e81fa45dc21474291cacdde4e4fef494470f
John Brooks <john@fastquake.com>
2019-08-04 17:21:24 -0400

    Initial commit

---
+++ conf/default.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Default settings for the bujomode plugin
+ *
+ * @author John Brooks <john@fastquake.com>
+ */
+
+//$conf['fixme']    = 'FIXME';
+
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the bujomode plugin
+ *
+ * @author John Brooks <john@fastquake.com>
+ */
+
+
+//$meta['fixme'] = array('string');
+
